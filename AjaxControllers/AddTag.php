7294cccdf56ab4d1e70b44c2b119f17aac35adfc
lucienchristiedervaux@gmail.com <lucienchristiedervaux@gmail.com>
2015-12-29 17:57:30 -0500

    Adding tags now works.

--- AjaxControllers/AddTag.php
+++ AjaxControllers/AddTag.php
@@ -1,6 +1,6 @@
 <?php
 //Working on tags.
-class AddTag extends AjaxController {
+class AddTagController extends AjaxController {
 	public function process($get,$post) {
 		/*if(!isset($post['tag_id'])||!is_numeric($post['tag_id'])||
 			!isset($post['course_id'])||!is_numeric($post['course_id'])||
@@ -8,20 +8,20 @@ class AddTag extends AjaxController {
 			$this->failureReason = 'Sorry, there was an error.';
 			return false;
 		}*/
-		$this->failureReason = 'Sorry, there was an error.';
+		//$this->failureReason = 'Sorry, there was an error.';
 		//$query = new Query('tagaction');
 		//Check if already added this course to tag action.
 		//echo "<h4> will add TAG </h4>"
 		// Now add this course to the user's model
+		
 		$action = new TagAction();
-		//$action->set('id',3);
 		$action->set('tag_id', $post['tag_id']);
 		$action->set('tag_name', $post['tag_name']);
 		$action->set('course_id', $post['course_id']);
-		$action->set('session_id', 1);
+		$action->set('session_id', "1");
 		$action->save();
 
-		return false;
+		return true;
 	}
 }
 
