cc12a35324d38d59921d5b244f229c6baa37faaf
Lucien Christie-Dervaux <chrisl2@rpi.edu>
2016-01-03 21:43:20 +0000

    more fixes attempts.

--- Static/PHP/connect.php
+++ Static/PHP/connect.php
@@ -1,4 +1,9 @@
 <?php
+echo $GLOBALS['CONFIG']['db-address'].'<br>';
+echo $GLOBALS['CONFIG']['db-username'].'<br>';
+echo $GLOBALS['CONFIG']['db-password'].'<br>';
+echo $GLOBALS['CONFIG']['db-database'].'<br>';
+var_dump(function_exists('mysqli_connect'));
 $GLOBALS['CONFIG']['mysqli'] = mysqli_connect($GLOBALS['CONFIG']['db-address'], 
 											  $GLOBALS['CONFIG']['db-username'], 
 											  $GLOBALS['CONFIG']['db-password'], 
