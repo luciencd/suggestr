03fed8f6c79c5050da89df47598e3fb708c3533b
lucienchristiedervaux@gmail.com <lucienchristiedervaux@gmail.com>
2016-01-26 19:16:36 -0500

    Major and Year inputs both work. Need to fix it so that they get fetched in the beginning.

--- AjaxControllers/AddSessionAspect.php
+++ AjaxControllers/AddSessionAspect.php
@@ -12,6 +12,7 @@ class AddSessionAspectController extends AjaxController {
 		}
 		
 		//In case we need to get the id of the major from a name.
+		//special case.
 		if($post['column'] == "major_name"){
 
 			//work out special cases.
@@ -19,8 +20,32 @@ class AddSessionAspectController extends AjaxController {
 			$result = $query->select('*', array(array('name', '=', $post['value'])), '', 2,true);
 			if(Count($result) > 0){
 				$department_id = $result[0]->get('id');
-			}	
+			}
+			$session = new Session();
+		
+			$session->findById($_COOKIE['sessionId']);
+			$session->set($post['column'], ucwords($department_id));
+			if(!is_numeric($department_id)){
+				$session->set("major_id", 0);
+				
+			}else{
+				$session->set("major_id", $department_id);
+			}
+			$session->set("major_input",$post['value']);
+			 // Just so that the ORM class thinks something's dirty and allows entry of an empty row
+			
+			$session->save();
+		}else{
+			//Other than that, edit table $post['table'] 's $post['id'] 'th column 
+			//by replacing $post['column'] with $post['value'];
+
+			$ORM = new session();
+			$ORM->findById($post['id']);
+			$ORM->set($post['column'], $post['value']);
+			$ORM->save();
 		}
+
+
 		
 
 
@@ -28,20 +53,7 @@ class AddSessionAspectController extends AjaxController {
 		
 		
 		// Now add this course to the user's model
-		$session = new Session();
 		
-		$session->findById($_COOKIE['sessionId']);
-		$session->set($post['column'], ucwords($department_id));
-		if(!is_numeric($department_id)){
-			$session->set("major_id", 0);
-			
-		}else{
-			$session->set("major_id", $department_id);
-		}
-		$session->set("major_input",$post['value']);
-		 // Just so that the ORM class thinks something's dirty and allows entry of an empty row
-		
-		$session->save();
 		return true;
 	}
 }
